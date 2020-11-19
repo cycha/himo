17631937a72cb181d4e641165166ba2c3cd4bb57
Charly Joulié <charly.joulie@gmail.com>
2020-11-19 01:24:06 +0100

    [bot] Add task clean db and add time interval to leboncoin task

--- bot/index.js
+++ bot/index.js
@@ -2,12 +2,14 @@ const leboncoin = require('./scrappers/leboncoin')
 const db = require('commons/db')
 const cron = require('node-cron');
 const proxy = require('./proxy');
+const Ad = require('commons/schema/schemaAd');
+const mdq = require('mongo-date-query');
 
 
 proxy.start();
 
 // Task to scrap le bon coin every 10 mins
-cron.schedule(' */10 * * * *', () => {
+cron.schedule(' 6-0/10 * * * *', () => {
     console.log("##################################################################");
     console.log('## TASK LEBONCOIN STARTING... ' + new Date().toISOString());
     console.log("##################################################################");
@@ -21,3 +23,18 @@ cron.schedule(' */10 * * * *', () => {
         })
         .then(() => db.close());
 });
+
+// Clean db once a month
+cron.schedule(' 0 0 1 * *', () => {
+    console.log("Cleaning db...");
+    db.connect()
+        .then(() => Ad.countDocuments({release_date: mdq.beforeLastYear()}))
+        .then(ads => console.log(ads))
+        .then(() => db.close());
+})
+
+// Handle exit
+const exitEvents = [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`,`SIGTERM`];
+exitEvents.forEach((eventType) => {
+    process.on(eventType, () => console.log("Bot terminated with " + eventType));
+})
